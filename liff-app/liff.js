a02e19a5ee11b4de3c781f4283abc75a9e0760af
ユーザ名mimatsu326 <michan326@nifty.com>
2018-12-05 00:45:24 +0900

    name削除

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -165,7 +165,7 @@ function liffRequestDevice() {
 
 function liffConnectToDevice(device) {
     device.gatt.connect().then(() => {
-        document.getElementById("device-name").innerText = device.name;
+        //document.getElementById("device-name").innerText = device.name;
         document.getElementById("device-id").innerText = device.id;
 
         // Show status connected
