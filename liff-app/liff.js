8ea9c3e9dc617a97e5b0f9d55751e563d6cfbe39
ユーザ名mimatsu326 <michan326@nifty.com>
2018-12-04 17:00:21 +0900

    tm[8]追加

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -249,7 +249,8 @@ function liffGetButtonStateCharacteristic(characteristic) {
             const el = document.getElementById("click-count");
             var lat_l = (val[3] << 24) + (val [2] << 16) + (val [1] << 8) + val [0];
             var lng_l = (val[7] << 24) + (val [6] << 16) + (val [5] << 8) + val [4];
-            el.innerText = lat_l + "," + lng_l;
+            var tm = val[8];
+            el.innerText = tm + ";" + lat_l + "," + lng_l;
 
         });
     }).catch(error => {
